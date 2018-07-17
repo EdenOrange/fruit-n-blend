e8e9f83281c2ad527dd420e27aa2c9704e5a97bc
leecopper15 <ntnicholasthie@gmail.com>
2018-07-17 15:15:51 +0700

    Added level select

--- States/levelSelect.js
+++ States/levelSelect.js
@@ -2,21 +2,43 @@ Game.LevelSelect = function(game) {
 
 }
 
+var levelsUnlocked = 3;
+
+var juiceStartX = 165;
+var juiceStartY = 420;
+var juiceMarginX = 130;
+var juiceMarginY = 215;
+
 Game.LevelSelect.prototype = {
     create: function() {
-        game.stage.backgroundColor = '#ff6622';
+        var background = game.add.image(0, 0, 'level_background');
 
-        var nameLabel = game.add.text(80, 80, 'Disillusion', {font: '50px Arial', fill: '#ffffff'});
-        var startLabel = game.add.text(80, game.world.height - 80, 'Press the "W" key to start', {font: '25px Arial', fill: '#ffffff'});
+        var title = game.add.image(game.world.width / 2, 100, 'level_title');
+        title.anchor.set(0.5);
 
-        var wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-        var eKey = game.input.keyboard.addKey(Phaser.Keyboard.E);
+        var page = game.add.image(game.world.width / 2, 600, 'level_page');
+        page.anchor.set(0.5);
 
-        wKey.onDown.addOnce(function(wKey){this.start(1)}, this);
-        eKey.onDown.addOnce(function(eKey){this.start(2)}, this);
+        for (let i = 0; i < 8; i++) {
+            var juiceX = juiceStartX + juiceMarginX * (i % 4);
+            var juiceY = juiceStartY + juiceMarginY * Math.floor(i / 4);
+            var juice;
+            if (i < levelsUnlocked) {
+                juice = game.add.image(juiceX, juiceY, 'level_juice');
+                juice.scale.set(1.05);
+                var text = game.add.text(juiceX, juiceY, (i+1).toString(), {font: 'bold 50px Poplar', fill: '#ffffff', stroke: '#000000', strokeThickness: 5});
+                text.anchor.set(0.5, -0.1);
+                juice.inputEnabled = true;
+                juice.events.onInputDown.addOnce(function(){this.loadLevel(i+1)}, this);
+            }
+            else {
+                juice = game.add.image(juiceX, juiceY, 'level_juice_empty');
+            }
+            juice.anchor.set(0.5);
+        }
     },
 
-    start: function(level) {
+    loadLevel: function(level) {
         game.state.start('game', true, false, level);
     }
 }
